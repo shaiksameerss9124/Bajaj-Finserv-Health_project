6cc586417bcf04f1529bae47b4f810b2af424aaf
Sameer Shaik <sameershaik209124@gmail.com>
2025-08-29 11:14:22 +0530

    Added spring boot

---
+++ src/main/java/example/javatask/javataskapplication.java
@@ -0,0 +1,65 @@
+package com.example.javatask;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class JavaTaskApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTaskApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        ObjectMapper mapper = new ObjectMapper();
+
+        String name = "Sameer Shaik";
+        String regNo = "REG12347";
+        String email = "sameer@example.com";
+
+        String url = "https://bfhldevapigw.healthrx.co.in/hiring/generateWebhook/JAVA";
+        String requestJson = String.format(
+                "{ \"name\": \"%s\", \"regNo\": \"%s\", \"email\": \"%s\" }",
+                name, regNo, email);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+        JsonNode root = mapper.readTree(response.getBody());
+        String webhookUrl = root.get("webhook").asText();
+        String accessToken = root.get("accessToken").asText();
+
+        System.out.println("Webhook URL: " + webhookUrl);
+
+        int lastTwo = Integer.parseInt(regNo.replaceAll("\\D", "")
+                .substring(regNo.replaceAll("\\D", "").length() - 2));
+
+        String finalQuery;
+        if (lastTwo % 2 == 1) {
+            finalQuery = "/* SQL Query for Question 1 */";
+        } else {
+            finalQuery = "/* SQL Query for Question 2 */";
+        }
+
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", accessToken);
+
+        String submitJson = String.format("{ \"finalQuery\": \"%s\" }", finalQuery);
+        entity = new HttpEntity<>(submitJson, headers);
+
+        ResponseEntity<String> submitResponse =
+                restTemplate.postForEntity(webhookUrl, entity, String.class);
+
+        System.out.println("Submission Response: " + submitResponse.getBody());
+    }
+}
